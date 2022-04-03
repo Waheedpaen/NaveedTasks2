ca1050c36288900f0b423d56ec72ea9f03cd1621
Waheedpaen <waheedpaen@gmail.com>
2022-04-03 18:10:24 +0500

    Update Method

--- src/app/getter-setter/getter-setter.module.ts
+++ src/app/getter-setter/getter-setter.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { GettersetterComponent } from './gettersetter/gettersetter.component';
 import { GettersetterRoutingmoduleModule } from './gettersetter-routingmodule/gettersetter-routingmodule.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MethodsComponent } from './methods/methods.component';
 
 
 
 @NgModule({
   declarations: [
-    GettersetterComponent
+    GettersetterComponent,
+    MethodsComponent
   ],
   imports: [
     CommonModule,
--- src/app/getter-setter/gettersetter-routingmodule/gettersetter-routingmodule.module.ts
+++ src/app/getter-setter/gettersetter-routingmodule/gettersetter-routingmodule.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GettersetterComponent } from '../gettersetter/gettersetter.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MethodsComponent } from '../methods/methods.component';
 
 const routes: Routes = [
-  {path:'getsetter',component:  GettersetterComponent }
+  {path:'getsetter',component:  GettersetterComponent },
+  {path:'loginData',component:  MethodsComponent }
 ];
 
 @NgModule({
