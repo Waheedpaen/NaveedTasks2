5e26559373e34020b6a8b1f30bc166bbae9d95e4
Waheedpaen <70056461+Waheedpaen@users.noreply.github.com>
2022-06-03 08:09:12 -0700

    updatedd

--- src/app/getter-setter/methods/methods.component.ts
+++ src/app/getter-setter/methods/methods.component.ts
@@ -7,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MethodsComponent implements OnInit {
 
-  constructor() { }
+  studentLIst:any=[
+    {id: 1,name: 'Waheed',age: 11,fees:1500,semester:6 },
+    {id: 2,name: 'Zubair',age: 12,fees:1600,semester:7 },
+    {id: 3,name: 'Ali',age: 13,fees:1700,semester:8 },
+    {id: 4,name: 'Usman',age: 14,fees:1800,semester:9 },
+    {id: 5,name: 'Abdullah',age: 15,fees:1900,semester:10 },
+  ]
+  constructor() {
+  console.log(this.studentLIst.slice(1,4))
+  this.studentLIst.push(
+    {id: 7,name: 'Abdullah7',age: 15,fees:1900,semester:10 },
+    {id: 8,name: 'Abdullah8',age: 15,fees:1900,semester:10 },
+    {id: 9,name: 'Abdullah9',age: 15,fees:1900,semester:10 },
+  )
+  this.studentLIst.unshift({id: 6,name: 'Waheed',age: 11,fees:1500,semester:6 },
+  {id: 7,name: 'Zubair',age: 12,fees:1600,semester:7 })
+this.studentLIst.reverse();
+this.studentLIst.include();
+this.studentLIst.filter(data=>data.name.startsWith('sam'))
+
+   }
 
   ngOnInit(): void {
   }
